9c4d264724d63c09bba0f7a1a78d05dcf189a686
Theo-Dor <75077536+Theo-Dor@users.noreply.github.com>
2021-01-20 23:30:27 +0600

    Initial commit

---
+++ Problems/Healthy sleep/main.py
@@ -0,0 +1,10 @@
+A = int(input())
+B = int(input())
+H = int(input())
+if H < A:
+    print("Deficiency")
+else:
+    if H > B:
+        print("Excess")
+    else:
+        print("Normal")
---
+++ Problems/Leap Year/main.py
@@ -0,0 +1,6 @@
+# put your python code here
+a = int(input())
+if (a % 4) == 0 and (a % 100) != 0 or (a % 400) == 0:
+    print("Leap")
+else:
+    print("Ordinary")
